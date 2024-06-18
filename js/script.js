f0f8e529d9ba3e40d9dea4d099a1281e880282f6
Hemang Kateshiya <hemang.kateshiya6@gmail.com>
2024-06-19 00:08:15 +0530

    fix: header changes

--- js/script.js
+++ js/script.js
@@ -1,36 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const calendar = new VanillaCalendar('#calendar', {
-        type: 'multiple',
-        today: new Date('2024-01-01'),
-        months: 12,
-        date: {
-            min: new Date().getFullYear() + '-01-01',
-            max: new Date().getFullYear() + '-12-31'
-        },
-        settings: {
-            visibility: {
-                theme: 'light',
-            },
-            selected: {
-                year: new Date().getFullYear(),
-                month: false,
-            },
-            selection: {
-                day: 'single',
-            },
-        },
-    });
-    calendar.init();
+document.addEventListener("DOMContentLoaded", () => {
+  const calendar = new VanillaCalendar("#calendar", {
+    type: "default",
+    date: {
+      min: new Date().getFullYear() + "-01-01",
+      max: new Date().getFullYear() + "-12-31",
+    },
+    settings: {
+      visibility: {
+        theme: "light",
+      },
+    },
+  });
+  calendar.init();
 });
 
-document.getElementById('input-search').addEventListener('focusin', (e) => {
-    let prefixSpan = document.getElementById('input-search-container');
-    prefixSpan.classList.add('shadow');
-    // let input = document.getElementById('input-search');
-    // input.classList.add('prefix-icon-border');
-})
+document.getElementById("input-search").addEventListener("focusin", (e) => {
+  let prefixSpan = document.getElementById("input-search-container");
+  prefixSpan.classList.add("shadow");
+  // let input = document.getElementById('input-search');
+  // input.classList.add('prefix-icon-border');
+});
 
-document.getElementById('input-search').addEventListener('focusout', (e) => {
-    let prefixSpan = document.getElementById('input-search-container');
-    prefixSpan.classList.remove('shadow');
-})
+document.getElementById("input-search").addEventListener("focusout", (e) => {
+  let prefixSpan = document.getElementById("input-search-container");
+  prefixSpan.classList.remove("shadow");
+});
